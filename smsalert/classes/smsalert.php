988e7d7147050fc4a34f7bc16924e17c7a005d08
Subashbist <63403403+Subashbist@users.noreply.github.com>
2020-04-14 18:02:14 +0530

    modify

--- smsalert/classes/smsalert.php
+++ smsalert/classes/smsalert.php
@@ -5,14 +5,25 @@
  */
  include_once("smsalert/helper/Utility.php");
  class Smsalert{
-	private $sender;       // declare senderid of user 
-	private $route;         // declare route of user 
-	private $url='http://www.smsalert.co.in';   // Define url 
-    private $authParams=array();
+    private $sender;       // declare senderid of user 
+    private $route;         // declare route of user 
+    private $url='http://www.smsalert.co.in';   // Define url
+    private $authParams=array();  // Define Authparams      
 
-    // function for sending smsalert
-	public function send($mobileno,$text,$schedule=null)
-	{	
+    //For Auth Params
+    public function getAuthParams()
+    {
+        if(empty($this->authParams))
+        {
+            die('Please Enter Apikey OR Username & password');
+        }else{
+            return $this->authParams;
+        }
+    }
+
+    // For Sending Smsalert
+    public function send($mobileno,$text,$schedule=null)
+    {   
         $url = $this->url.'/api/push.json';
         $params=array('sender'=>$this->sender,'mobileno'=>$mobileno,'text'=>$text);   
         if(!empty($schedule))
@@ -23,43 +34,43 @@
         {
             $params['route']=$this->route; 
         }
-        $params = array_merge($params,$this->authParams);
+        $params = array_merge($params,$this->getAuthParams());
         return Utility::invoke_api($url,$params);
     }
-   
-    // function for Sender Id List
+    
+    // For Edit Scheudle
+    public function editSchedule($batchid,$schedule)
+    {
+        $url = $this->url.'/api/modifyschedule.json';
+        $params=array('batchid'=>$batchid,'schedule'=>$schedule);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    // For Cancel Scheudle
+    public function cancelSchedule($batchid)
+    {
+        $url = $this->url.'/api/cancelschedule.json';
+        $params=array('batchid'=>$batchid);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    // For Sender Id List
     public function getSenderId()
     {
-    	$url = $this->url.'/api/senderlist.json';
-		return Utility::invoke_api($url,$this->authParams);
+        $url = $this->url.'/api/senderlist.json';
+        return Utility::invoke_api($url,$this->getAuthParams());
     }
 
-    // function for user profile
+    // For User Profile
     public function getUserProfile()
     {
-    	$url = $this->url.'/api/user.json';
-		return Utility::invoke_api($url,$this->authParams);
+        $url = $this->url.'/api/user.json';
+        return Utility::invoke_api($url,$this->getAuthParams());
     }
 
-    //function for group list
-    public function getGroupList($limit=10,$page=1,$order='desc')
-    {
-    	$url = $this->url.'/api/grouplist.json';
-    	$params=array('limit'=>$limit,'page'=>$page,'order'=>$order);
-        $params = array_merge($params,$this->authParams);
-		return Utility::invoke_api($url,$params);
-    }
-
-    //function for contact list
-    public function getContactList($groupid=null,$limit=10,$page=1,$order='desc')
-    {
-    	$url = $this->url.'/api/grouplist.json';
-    	$params=array('group_id'=>$groupid,'limit'=>$limit,'page'=>$page,'order'=>$order);
-        $params = array_merge($params,$this->authParams);
-		return Utility::invoke_api($url,$params);
-    }
-
-    //function for send sms using xml
+    // For Send Sms Using xml
     public  function sendSmsXml($sms_datas)
     {   if(is_array($sms_datas) && sizeof($sms_datas) == 0)
         {return false;}
@@ -87,66 +98,270 @@ XML;
         if($msg->count() <= 1)
         { return false; }         
         $xmldata = $msg->asXML();
-        $url = 'http://www.smsalert.co.in/api/xmlpush.json';
-        $params=array(
-        'data'=>$xmldata); 
+        $url = $this->url.'/api/xmlpush.json';
+        $params=array('data'=>$xmldata); 
         return Utility::invoke_api($url,$params);
     }
 
-    //function to Create Contact
+    //For Group List
+    public function getGroupList($limit=10,$page=1,$order='desc')
+    {
+        $url = $this->url.'/api/grouplist.json';
+        $params=array('limit'=>$limit,'page'=>$page,'order'=>$order);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Create Group
+    public function createGroup($grpname)
+    {
+        $url = $this->url.'/api/creategroup.json';
+        $params=array('name'=>$grpname);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Delete Group
+    public function deleteGroup($id)
+    {
+        $url = $this->url.'/api/deletegroup.json';
+        $params=array('id'=>$id);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Edit Group
+    public function editGroup($grpname,$id)
+    {
+        $url = $this->url.'/api/updategroup.json';
+        $params=array('id'=>$id,'name'=>$grpname);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Send Group Sms
+    public function sendGroupSms($grpname,$grid,$text,$schedule=null)
+    {
+        $url = $this->url.'/api/grouppush.json';
+        $params=array('id'=>$grid,'name'=>$grpname,'text'=>$text,'schedule'=>$schedule,'sender'=>$this->sender);
+        $params['route']= !empty($this->route) ? $this->route : '';
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Contact List
+    public function getContactList($groupid,$limit=10,$page=1,$order='desc')
+    {
+        $url = $this->url.'/api/contactlist.json';
+        $params=array('group_id'=>$groupid,'limit'=>$limit,'page'=>$page,'order'=>$order);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+
+    //For Create Contact
     public function createContact($grpname,$name,$number)
     {
         $url = $this->url.'/api/createcontact.json';
         $params=array('grpname'=>$grpname,'name'=>$name,'number'=>$number);
-        $params = array_merge($params,$this->authParams);
+        $params = array_merge($params,$this->getAuthParams());
         return Utility::invoke_api($url,$params);
     }
 
-    //function to create group
-    public function createGroup($grpname)
+    //For Edit Contact
+    public function editContact($id,$name,$number)
     {
-        $url = $this->url.'/api/creategroup.json';
-        $params=array(
-        'name'=>$grpname);
-        $params = array_merge($params,$this->authParams);
+        $url = $this->url.'/api/updatecontact.json';
+        $params=array('id'=>$id,'name'=>$name,'number'=>$number);
+        $params = array_merge($params,$this->getAuthParams());
         return Utility::invoke_api($url,$params);
     }
 
-    //function to get template list
+    //For Delete Contact
+    public function deleteContact($id)
+    {
+        $url = $this->url.'/api/deletecontact.json';
+        $params=array('id'=>$id);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);
+    }
+    //For Contat create with xml
+    public function importXmlContact($sms_datas=null,$grpname)
+    {
+       if(is_array($sms_datas) && sizeof($sms_datas) == 0)
+        {return false;}
+        $xmlstr = <<<XML
+<?xml version='1.0' encoding='UTF-8'?>
+<group>
+</group>
+XML;
+        $msg = new SimpleXMLElement($xmlstr);
+        $user = $msg->addChild('user');
+        if (array_key_exists("apikey",$this->authParams))
+        {
+            $user->addAttribute('apikey', $this->authParams['apikey']);  
+        }else{
+            $user->addAttribute('username', $this->authParams['user']);
+            $user->addAttribute('password', $this->authParams['pwd']); 
+        }  
+        $user->addAttribute('grp_name',$grpname);
+        $members = $msg->addChild('members');
+        foreach($sms_datas as $sms_data)
+        {
+            $member = $members->addChild('member');
+            $member->addAttribute('name', $sms_data['person_name']);
+            $member->addAttribute('number', $sms_data['number']);
+        }   
+        if($msg->count() <= 1)
+        { return false; }         
+        $xmldata = $msg->asXML();
+        $url = $this->url.'/api/createcontactxml.json';
+        $params=array('data'=>$xmldata); 
+        return Utility::invoke_api($url,$params);
+    }
+
+    //Template List
     public function getTemplateList()
     {
         $url = $this->url.'/api/templatelist.json';      
-        return Utility::invoke_api($url,$this->authParams);
+        return Utility::invoke_api($url,$this->getAuthParams());
     }
 
-    //function to set apikey
+    //Create Template
+    public function createTemplate($name,$text)
+    {
+        $url = $this->url.'/api/createtemplate.json'; 
+        $params=array('name'=>$name,'text'=>$text);
+        $params = array_merge($params,$this->getAuthParams());     
+        return Utility::invoke_api($url,$params);
+    }
+    
+    //Edit Template
+    public function editTemplate($name,$text,$id)
+    {
+        $url = $this->url.'/api/updatetemplate.json'; 
+        $params=array('name'=>$name,'text'=>$text,'id'=>$id);
+        $params = array_merge($params,$this->getAuthParams());     
+        return Utility::invoke_api($url,$params);
+    }
+
+    //Edit Template
+    public function deleteTemplate($id)
+    {
+        $url = $this->url.'/api/deletetemplate.json'; 
+        $params=array('id'=>$id);
+        $params = array_merge($params,$this->getAuthParams());     
+        return Utility::invoke_api($url,$params);
+    }
+
+    //Check Balance
+    public function balanceCheck()
+    {
+        $url = $this->url.'/api/creditstatus.json';
+        return Utility::invoke_api($url,$this->getAuthParams());
+    }
+
+    //Update Profile
+    public function updateProfile($fname,$lname,$number,$emailid)
+    {
+        $url = $this->url.'/api/updateprofile.json';
+        $params=array('firstname'=>$fname,'lastname'=>$lname,'mobilenumber'=>$number,'emailid'=>$emailid);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params);   
+    }
+
+    //Generate Otp
+    //mandatory to include [otp] tag in template content
+    public function generateOtp($mobileno,$template)
+    {
+        $url = $this->url.'/api/mverify.json';
+        $params=array('sender'=>$this->sender,'mobileno'=>$mobileno,'template'=>$template);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    //Validate Otp
+    public function validateOtp($mobileno,$code)
+    {
+        $url = $this->url.'/api/mverify.json';
+        $params=array('code'=>$code,'mobileno'=>$mobileno);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    // Create Short Url
+    public function createShortUrl($longurl)
+    {
+        $url = $this->url.'/api/createshorturl.json';
+        $params=array('url'=>$longurl);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    // Create Short Url
+    public function deleteShortUrl($urlid)
+    {
+        $url = $this->url.'/api/deleteshorturl.json';
+        $params=array('id'=>$urlid);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    //Sent Sms Report
+    public function smsReport($limit=10,$page=1,$schedule=1)
+    {
+        $url = $this->url.'/api/smscampaignlog.json';
+        $params=array('limit'=>$limit,'page'=>$page,'schedule'=>$schedule);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    //Push Report
+    public function pushReport($mobileno,$text,$reference,$dlrurl,$schedule=null)
+    {
+        $url = $this->url.'/api/push.json';
+        $dlrurl = (parse_url($dlrurl, PHP_URL_HOST) == 'localhost') ? urlencode($dlrurl) : $dlrurl;   
+        $params=array('sender'=>$this->sender,'mobileno'=>$mobileno,'text'=>$text,
+                      'reference'=>$reference, 'dlrurl'=>$dlrurl,'schedule'=>$schedule);
+        $params['route']= !empty($this->route) ? $this->route : '';
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    //Pull Report
+    public function pullReport($batchid)
+    {   
+        $url = $this->url.'/api/pull.json';
+        $params=array('batchid'=>$batchid);
+        $params = array_merge($params,$this->getAuthParams());
+        return Utility::invoke_api($url,$params); 
+    }
+
+    //Set Apikey
     function authWithApikey($apikey)
     {
         $this->authParams=array('apikey'=>$apikey);
         return $this;
     }
 
-    //function to set apikey
+    //Set Username and Password
     function authWithUserIdPwd($user,$pwd)
     {
         $this->authParams=array('user'=>$user,'pwd'=>$pwd);
         return $this;
     }
 
-    //function for set route
+    //Set Route
     public function setRoute($route)
     {
-    	$this->route = $route;
-    	return $this;
+        $this->route = $route;
+        return $this;
     } 
 
-    //function for set senderid
+    //Set Senderid
     public function setSender($sender)
     {
-    	$this->sender = $sender;
-    	return $this;
+        $this->sender = $sender;
+        return $this;
     } 
 }
-
-
 ?>
