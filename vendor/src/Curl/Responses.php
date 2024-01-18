30aa76106489ee30d406752c5d78f7f5d70e30ca
Shweta Singh <103027323+shwetahub@users.noreply.github.com>
2024-01-18 16:00:56 +0530

    Update Responses.php
    Add HTTP code

--- vendor/src/Curl/Responses.php
+++ vendor/src/Curl/Responses.php
@@ -2,15 +2,21 @@
 namespace SMSAlert\Lib\Curl;
 Class Responses{
 	private $resp_body = '';
+	private $http_code = '';
 	
-	public function __construct($response)
+	public function __construct($response, $http_code)
 	{
 		$this->resp_body=$response;
+		$this->http_code=$http_code;
 	}
 	public function getBody()
 	{
 		return $this->resp_body;
 		
+	}
+	public function getStatusCode()
+	{
+		return $this->http_code;
 	}
 	
 }
